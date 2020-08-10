c25cf704c16ff0430459873fa52772822d2d933f
anjanajanghu <47345471+anjanajanghu@users.noreply.github.com>
2020-08-10 14:43:52 +0530

    added command objects and converter

--- src/main/java/com/example/Recipe/Recipe/converters/CategoryCommandToCategory.java
+++ src/main/java/com/example/Recipe/Recipe/converters/CategoryCommandToCategory.java
@@ -1,5 +1,28 @@
 package com.example.Recipe.Recipe.converters;
 
-public class CategoryCommandToCategory {
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
 
+import com.example.Recipe.Recipe.command.CategoryCommand;
+import com.example.Recipe.Recipe.domain.Category;
+
+import lombok.Synchronized;
+
+@Component
+public class CategoryCommandToCategory implements Converter<CategoryCommand, Category>{
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Category convert(CategoryCommand source) {
+        if (source == null) {
+            return null;
+        }
+
+        final Category category = new Category();
+        category.setId(source.getId());
+        category.setDescription(source.getDescription());
+        return category;
+    }
 }
--- src/main/java/com/example/Recipe/Recipe/converters/IngredientCommandToIngredient.java
+++ src/main/java/com/example/Recipe/Recipe/converters/IngredientCommandToIngredient.java
@@ -1,5 +1,33 @@
 package com.example.Recipe.Recipe.converters;
 
-public class IngredientCommandToIngredient {
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
 
+import com.example.Recipe.Recipe.command.IngredientCommand;
+import com.example.Recipe.Recipe.domain.Ingredient;
+
+@Component
+public class IngredientCommandToIngredient implements Converter<IngredientCommand, Ingredient> {
+
+    private final UnitOfMeasureCommandToUnitOfMeasure uomConverter;
+
+    public IngredientCommandToIngredient(UnitOfMeasureCommandToUnitOfMeasure uomConverter) {
+        this.uomConverter = uomConverter;
+    }
+
+    @Nullable
+    @Override
+    public Ingredient convert(IngredientCommand source) {
+        if (source == null) {
+            return null;
+        }
+
+        final Ingredient ingredient = new Ingredient();
+        ingredient.setId(source.getId());
+        ingredient.setAmount(source.getAmount());
+        ingredient.setDescription(source.getDescription());
+        ingredient.setUom(uomConverter.convert(source.getUnitOfMeasure()));
+        return ingredient;
+    }
 }
